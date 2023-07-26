e1bcd8c9d6a288696ed1041c8fc195fd0afbf450
KamilSliwka <kamilsliwinski81@gmail.com>
2023-07-26 21:34:12 +0200

    clean code

--- main.cpp
+++ main.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
 #include <cmath>
+#include "cmake-build-debug/PrimeGenerator.h"
 
 using namespace std;
 
 int main() {
-    cout<<"podaj liczbe: "<<endl;
+    cout<<"Give a number: "<<endl;
     int upperLimit ;
     cin>>upperLimit;
 
-    bool *isPrimeNumber;
-    isPrimeNumber = new bool[upperLimit];
-    for(int i = 0;i<upperLimit;i++){
-        isPrimeNumber[i]=true;
-    }
-    isPrimeNumber[0]=false;
-    isPrimeNumber[1]=false;
-    for(int i = 2;i<sqrt(upperLimit);i++){
-        if(isPrimeNumber[i]==true){
-            for(int j = i+1;j < upperLimit;j++){
-                if(j%i==0){
-                    isPrimeNumber[j]=false;
-                }
-            }
-        }
-    }
-    for(int i = 0;i<upperLimit;i++){
-        if(isPrimeNumber[i]==true){
-            cout<< i <<" ";
-        }
-    }
+    PrimeGenerator generator(upperLimit);
+    generator.generatePrimes();
+    generator.showPrimes();
+
     return 0;
 }
